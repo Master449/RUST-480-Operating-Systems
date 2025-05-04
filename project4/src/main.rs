56c0d486b31628e4500fa82ff9750f2d774e4438
Master449 <20436145+Master449@users.noreply.github.com>
2025-05-03 21:00:34 -0500

    Started importing the helper functions, and fleshed out the dump_queue methods. Added .rustfmt.toml to configure the formatter to stop being annoying half the time.

--- project4/src/main.rs
+++ project4/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::VecDeque;
 use std::env;
 use std::fs::{read, read_to_string};
 use std::process::exit;
@@ -18,7 +19,6 @@ struct Process {
     wait_time: i32,
 }
 
-#[rustfmt::skip]
 impl Process {
     fn debug_info(&self) {
         println!("Name:          {}", self.name);
@@ -43,7 +43,6 @@ impl Process {
     }
 }
 
-#[rustfmt::skip]
 fn main() {
     const DEBUG_FLAG: bool = true;
 
@@ -55,7 +54,9 @@ fn main() {
         exit(1)
     }
 
-    if DEBUG_FLAG { println!("PASSED ARGS"); }
+    if DEBUG_FLAG {
+        println!("PASSED ARGS");
+    }
 
     // Get lines of input
     let input: Vec<String> = read_to_string(&args[1])
@@ -68,23 +69,35 @@ fn main() {
 
     while i < input.len() {
         if input[i].contains("STOPHERE") {
-            if DEBUG_FLAG { println!("STOPHERE ENCOUNTERED"); }
+            if DEBUG_FLAG {
+                println!("STOPHERE ENCOUNTERED");
+            }
             break;
         }
-        if DEBUG_FLAG { println!("EXITED STOPHERE"); }
+        if DEBUG_FLAG {
+            println!("EXITED STOPHERE");
+        }
 
         // First line is program name and id
         let input_name_and_id: Vec<String> = input[i].split(' ').map(String::from).collect();
-        let name_and_id: Vec<String> = input_name_and_id.into_iter().filter(|s| !s.is_empty()).collect();
+        let name_and_id: Vec<String> = input_name_and_id
+            .into_iter()
+            .filter(|s| !s.is_empty())
+            .collect();
 
         // Increment because
         i += 1;
 
         // Second line is program history
         let history_strings: Vec<String> = input[i].split(' ').map(String::from).collect();
-        let history: Vec<String> = history_strings.into_iter().filter(|s| !s.is_empty()).collect();
+        let history: Vec<String> = history_strings
+            .into_iter()
+            .filter(|s| !s.is_empty())
+            .collect();
 
-        if DEBUG_FLAG { println!("STARTING PROCESS HISTORY STRING PROCESSING"); }
+        if DEBUG_FLAG {
+            println!("STARTING PROCESS HISTORY STRING PROCESSING");
+        }
 
         // tmp variables for processing the instructions to
         // Vec<(Instruction, Burst) ...> format
@@ -101,7 +114,7 @@ fn main() {
             // If the instructions start with N, its cutoff time
             if char_str == "N" {
                 break;
-            } 
+            }
 
             if flip_flop {
                 // if flip flop is true, then we have the instruction already
@@ -116,24 +129,110 @@ fn main() {
             }
         }
 
-        if DEBUG_FLAG { println!("ATTEMPTING CREATE PROCESS STRUCT"); }
+        if DEBUG_FLAG {
+            println!("ATTEMPTING CREATE PROCESS STRUCT");
+        }
 
+        let proc = create_process(name_and_id[0].clone(), i as i8, name_and_id[1].parse().unwrap(), instruction_set);
         // Create process based on the input strings we gathered
-        let proc = Process {
-            name: name_and_id[0].clone(),
-            id: (i as i8),
-            arrival_time: name_and_id[1].parse().unwrap(),
-            history: instruction_set,
-            history_index: 0,
-            cpu_timer: 0,
-            cpu_total: 0,
-            cpu_burst_count: 0,
-            io_timer: (0, 0),
-            io_total: (0, 0),
-            io_burst_count: (0, 0),
-            end_time: 0,
-            wait_time: 0,
-        };
         proc.debug_info();
     }
 }
+
+#[rustfmt::skip]
+fn create_process(process_name: String, process_id: i8, arrival: i32, instructions: Vec<(String, i32)>) -> Process {
+    Process {
+        name: process_name,
+        id: process_id,
+        arrival_time: arrival,
+        history: instructions,
+        history_index: 0,
+        cpu_timer: 0,
+        cpu_total: 0,
+        cpu_burst_count: 0,
+        io_timer: (0, 0),
+        io_total: (0, 0),
+        io_burst_count: (0, 0),
+        end_time: 0,
+        wait_time: 0,
+    }
+}
+
+/* dump_queue
+ *    takes a reference to a queue and prints all the process
+ *    IDs that are inside. Mainly for the summaries.
+ *
+ * Args
+ *   q    - reference to a queue
+ *   name - name of the queue to print
+ ****************************************************************/
+fn dump_queue(q: &VecDeque<Process>, name: String) {
+    print!("{name} Queue Contents: ");
+    if q.is_empty() {
+        print!("(Empty)");
+    } else {
+        for item in q {
+            print!("{0} ", item.id);
+        }
+    }
+    println!();
+}
+
+/* dump_all_queues
+ *    Just prints out every queue sequentially
+ *
+ * Args
+ *   all - reference Vec holding all VecDeques
+ * **************************************************/
+fn dump_all_queues(all: &Vec<VecDeque<Process>>) {
+    let names = ["Entry", "Ready", "Input", "Output"];
+    for i in 1..4 {
+        dump_queue(&all[i], names[i].to_string());
+    }
+}
+
+/* update_work_status
+ *    checks if a process is at the end of its history, and terminates if so.
+ *    if its not, updates the timers for the work to be done, puts it in
+ *    the correct queue for processing, and prints out the change.
+ *
+ * Args
+ *   proc - reference to process
+ * ******************************************************************************/
+fn update_work_status(proc: &mut Process, timer: i32) {
+    if (proc.history_index as usize) == proc.history.len() - 1 {
+        proc.end_time = timer + 1;
+        proc.wait_time = (proc.end_time - proc.arrival_time) - proc.cpu_total - proc.io_total.0 - proc.io_total.1;
+    }
+}
+
+/* check_num_process
+ *    checks to see if there is room to add any processes
+ *    from the entryq to the readyq
+ * ***************************************************************/
+fn check_num_process() {}
+
+/* process_active
+ *    processes the CPUs bursts. If the burst reaches 0
+ *    sends it to update_work_status to see if it
+ *    stops or moves onto bigger and better things
+ *
+ *    if nothing is here it adds idle time to the total
+ * ***************************************************************/
+fn process_active(proc: &mut Process) {}
+
+/* process_iactive
+ *    processes the input burst. If the burst reaches 0
+ *    sends it to update_work_status to see if it
+ *    stops or moves onto bigger and better things
+ *
+ * ***************************************************************/
+fn process_iactive(proc: &mut Process) {}
+
+/* process_oactive
+ *    processes the output burst. If the burst reaches 0
+ *    sends it to update_work_status to see if it
+ *    stops or moves onto bigger and better things
+ *
+ * ***************************************************************/
+fn process_oactive(proc: &mut Process) {}
